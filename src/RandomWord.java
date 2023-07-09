9f081e5c5b707bf0f823096592c652c4b06d6782
rezeil3 <elidejano@gmail.com>
2023-07-09 21:37:24 +0800

    Formula working, Input not working

--- src/RandomWord.java
+++ src/RandomWord.java
@@ -2,23 +2,30 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
-import java.util.Scanner;
-
 public class RandomWord {
     public static void main(String[] args) {
-        String[] wordsArray = {"a", "b", "c"};
-            //String wordsString = StdIn.readString();
-            //String[] wordsArray = wordsString.split(" ");
-            int n = wordsArray.length;
-            String champion = null;
 
-            for (int i = 0; i < n; i++) {
-                double p = 1.0 / ((double) i + 1.0);
-                if (StdRandom.bernoulli(p)) {
-                   champion = wordsArray[i];
-                }
+        //Accepts a line of words
+        String wordsString = StdIn.readLine();
+
+        //Converts the line into an array separated by space
+        String[] wordsArray = wordsString.split(" ");
+
+        //Variable for the number of words
+        int n = wordsArray.length;
+
+        //Variable for the winning word
+        String champion = null;
+
+        //Knuth's method using bernoulli as the randomizer
+        for (int i = 0; i < n; i++) {
+            double p = 1 / ((double) i + 1);
+            //Changes the winning word when returned true
+            if (StdRandom.bernoulli(p)) {
+                champion = wordsArray[i];
             }
-            StdOut.println(champion);
+        }
+        //Prints out the winning word
+        StdOut.println(champion);
     }
 }
-
